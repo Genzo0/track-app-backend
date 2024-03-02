7ba686f9a194b494300495d54b0c91693d682849
Alifyan <genzo@pop-os.localdomain>
2024-03-02 13:11:08 +0700

    edit getResibynoresi function

--- controllers/resiController.js
+++ controllers/resiController.js
@@ -15,6 +15,9 @@ const getResiByNoResi = async (req, res) => {
     if (!resi) {
       return res.status(404).json({ message: "Resi tidak ditemukan" });
     }
+    if (resi.isAccepted) {
+      return res.status(400).json({ message: "Resi sudah diterima" });
+    }
     res.json(resi);
   } catch (error) {
     res.status(500).json({ message: error.message });
